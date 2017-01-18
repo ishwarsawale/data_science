b838f9da691399a9ad7136f9615b3e2fe8a7cf4f
machine <ansh.iam@gmail.com>
2017-01-18 16:14:51 +0530

    england test match analyasis

---
+++ get_data.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+url = 'http://stats.espncricinfo.com/ci/engine/team/7.html?class=1;opposition=1;template=results;type=team;view=results'
+
+r = requests.get(url)
+html = r.text
+
+#create soup object
+soup = BeautifulSoup(html,'lxml')
+recs = soup.select('tbody > .data1')
+
+file = open('data_england_test.csv', "w")
+for i in range(2,len(recs)):
+        single = recs[i].findAll('td')
+        file.write(single[0].text + ','+single[1].text+ ','+single[2].text+ ','+single[3].text+ ','+ single[4].text+ ','+single[6].text+ ','+ single[7].text+'\n')
+
+file.close()
